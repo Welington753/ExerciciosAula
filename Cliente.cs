3107e510d287ecde3d16dc801a13e98c4f5a665e
Welington Maciel <wwluiz123@gmail.com>
2026-04-21 00:02:00 -0300

    Descrição das alterações

---
+++ Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosAulas
+{
+     class Cliente
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace ExerciciosAulas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Cliente[] quantidade = new Cliente[10];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("Rent #" + (i + 1) + ":");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                quantidade[room] = new Cliente { Name = name, Email = email, Room = room };
+
+                Console.WriteLine("Cliente registrado: " + quantidade[room].Name + ", " + quantidade[room].Email + ", Room: " + quantidade[room].Room);
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < quantidade.Length; i++)
+            {
+                if (quantidade[i] != null)
+                {
+                    Console.WriteLine("Room " + i + ": " + quantidade[i].Name + ", " + quantidade[i].Email);
+                }
+            }
+        }
+    }
+}
